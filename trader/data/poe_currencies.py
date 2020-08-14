1101578abbd9e5f7d508276aaea5d2c4cbf6afd5
NitinJ <nitin.jaglan@gmail.com>
2020-08-14 13:53:45 +0530

    Currency/Item changes to create items from clipboard
    Improved item parsing from clipboard. Changed currency and item classes and corresponding changes in currency exchange to create currency objects

--- trader/data/poe_currencies.py
+++ trader/data/poe_currencies.py
@@ -1,61 +1,26 @@
 from poe_item import *
 from enum import Enum
+from network.poe_api import PoeApi
 
-from currency_exchange import CurrencyExchange
-
-class CurrencyType(Enum):
-    UNKNOWN = 0
-    CHAOS = 1
-    EXALT = 2
-
-class Currency:
-    def __init__(self, name, max_stack_in_trade):
+class Currency():
+    def __init__(self, ex, name, trade_name, max_stack_in_trade):
+        self.exchange = ex
         self.name = name
+        self.trade_name = trade_name
         self.max_stack_in_trade = max_stack_in_trade
-        
-        self.type = self._get_type()
-        self.trade_name = self._get_trade_name()
-        
-        self.exchange = CurrencyExchange.get_instance()
-
-    def _get_trade_name(self):
-        if self.type == CurrencyType.CHAOS:
-            return "chaos"
-        else:
-            return "exalted"
-
-    def _get_type(self):
-        if self.name == "Chaos Orb":
-            return CurrencyType.CHAOS
-        elif self.name == "Exalted Orb":
-            return CurrencyType.EXALT
-        raise Exception("Currency not supported !")
 
     def get_value_in_chaos(self):
-        if self.type == CurrencyType.CHAOS:
-            return 1
-        else:
-            return self.exchange.get_exchange_rate(self)
-
-    @staticmethod
-    def create(type):
-        if type == CurrencyType.CHAOS:
-            return Currency("Chaos Orb", 10)
-        else:
-            return Currency("Exalted Orb", 10)
-        raise Exception("Currency not supported !")
+        return self.exchange.get_exchange_rate(self)
 
     def __str__(self):
-        return "{} : {}".format(self.name, self.type)
+        exchange_rate = self.get_value_in_chaos()
+        return "name: {}, trade_name: {}, max_stack: {}, value_in_chaos: {}".format(
+            self.name,
+            self.trade_name,
+            self.max_stack_in_trade,
+            exchange_rate if exchange_rate else "unknown")
 
-class CurrencyItem(Item):
-    def __init__(self, currency, quantity):
-        self.currency = currency
-        self.quantity = quantity
-        Item.__init__(self, self.currency.name, ItemType.CURRENCY)
-
-    def get_value_in_chaos(self):
-        return self.currency.get_value_in_chaos() * self.quantity
-
-    def __str__(self):
-        return self.currency.__str__() + ", quantity:" + self.quantity
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return other.name == self.name and other.trade_name == self.trade_name
+        return False
