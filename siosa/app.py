dd74b6d04ad5347aabd0116a7d1d0581ab077ae9
NitinJ <nitin.jaglan@gmail.com>
2020-08-15 04:08:42 +0530

    Update app.py
    Updated app file to create config object

--- siosa/app.py
+++ siosa/app.py
@@ -3,34 +3,35 @@ import Queue
 import threading
 import time
 from time import sleep
+import os
 
 from pygtail import Pygtail
 
-from siosa.client.log_listener import ClientLogListener
 from siosa.clipboard.poe_clipboard import PoeClipboard
-from siosa.control.game_controller import GameController
-from siosa.control.game_state import GameState
-from siosa.control.game_state_updater import GameStateUpdater
 from siosa.data.currency_exchange import CurrencyExchange
-from siosa.data.poe_currencies import *
-from siosa.data.poe_item_factory import PoeItemFactory
-from siosa.data.stash import Stash
-from siosa.location.location_factory import LocationFactory, Locations
 from siosa.network.poe_api import PoeApi
-from siosa.trader.trade_controller import TradeController
-from siosa.trader.trade_request import TradeRequest
+from siosa.control.steps.scan_inventory_step import ScanInventory
+from siosa.control.init_task import InitTask
+from siosa.control.game_state import GameState
+from siosa.config.siosa_config import SiosaConfig
 
 FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
 logging.basicConfig(format=FORMAT)
 
 
 def run():
-    api = PoeApi("MopedDriverr", "952f84865d28d000cba67dcb1d044914")
+    config_file_path = os.path.join(os.path.dirname(__file__), "config.json")
+    config = SiosaConfig(config_file_path).config
+    
+    api = PoeApi(config['account_name'], config['poe_session_id'])
     exchange = CurrencyExchange(api)
-    poe_clipboard = PoeClipboard()
-    while True:
-        time.sleep(1)
-        poe_clipboard.read_item_at_cursor()
+    # poe_clipboard = PoeClipboard()
+    # while True:
+    #     poe_clipboard.read_item_at_cursor()
+    
+    # time.sleep(1)
+    # step = ScanInventory({})
+    # step.execute()
 
 
 if __name__ == "__main__":
