d1d0a37e841f2c41bf9172b2b7b1d04d3f608933
NitinJ <nitin.jaglan@gmail.com>
2020-08-16 18:05:22 +0530

    Update clipboard_reader.py
    Updated clipboard reader to catch access denied exceptions while reading clipboard and try again.

--- siosa/clipboard/clipboard_reader.py
+++ siosa/clipboard/clipboard_reader.py
@@ -1,10 +1,14 @@
 import win32clipboard
+import logging
+
 from siosa.common.singleton import Singleton
 
 
 class ClipboardReader(Singleton):
     def __init__(self):
         Singleton.__init__(self)
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel('DEBUG')
         self.last_read_data = None
         self.last_write_data = None
         self.previously_written_data = None
@@ -22,10 +26,15 @@ class ClipboardReader(Singleton):
         return data
 
     def _read_internal(self):
-        win32clipboard.OpenClipboard()
-        data = win32clipboard.GetClipboardData()
-        win32clipboard.CloseClipboard()
-        return data
+        while True:
+            try:
+                win32clipboard.OpenClipboard()
+                data = win32clipboard.GetClipboardData()
+                win32clipboard.CloseClipboard()
+                return data
+            except Exception as e:
+                self.logger.error("Error while reading clipboard: {}".format(str(e)))
+                
 
     def _write_internal(self, data):
         win32clipboard.OpenClipboard()
