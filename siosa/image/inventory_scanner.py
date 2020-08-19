ec3ba0a979b4fe86488efe334462d1f3fc994871
NitinJ <nitin.jaglan@gmail.com>
2020-08-20 04:02:54 +0530

    Update inventory_scanner.py
    Fixed inventory scanner to properly return items based on their position in the inventory. This is more robust than before as it takes into account border between different inventory cells (3px)

--- siosa/image/inventory_scanner.py
+++ siosa/image/inventory_scanner.py
@@ -1,23 +1,37 @@
+import logging
+
 from siosa.image.template_matcher import TemplateMatcher
 from siosa.location.location_factory import Locations
 
 
 class InventoryScanner:
+    ROWS = 5
+    COLUMNS = 12
+
     def __init__(self):
-        self.tm = TemplateMatcher(Locations.INVENTORY_0_0, debug=True)
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel('DEBUG')
+        self.tm = TemplateMatcher(Locations.INVENTORY_0_0, debug=False)
 
     def scan(self):
         points = self.tm.match(Locations.INVENTORY)
         return self._convert_points_to_inventory_positions(points)
 
     def _convert_points_to_inventory_positions(self, points):
-        min_x = min([p[0] for p in points])
-        min_y = min([p[1] for p in points])
+        center00_x, center00_y = Locations.INVENTORY_0_0.get_center()
+        offset_x, offset_y = (
+            center00_x - Locations.INVENTORY.x1, center00_y - Locations.INVENTORY.y1)
         width = Locations.INVENTORY_0_0.get_width()
         height = Locations.INVENTORY_0_0.get_height()
-        positions = {}
-        for p in points:
-            pos_x = (p[1] - min_y) // height
-            pos_y = (p[0] - min_x) // width
-            positions[(pos_x, pos_y)] = 1
-        return positions
+
+        positions_of_missing_items = [(abs(p[1] - offset_y)//height, abs(p[0] - offset_x)//width)
+                                      for p in points]
+        item_positions = {}
+        for i in range(0, InventoryScanner.ROWS):
+            for j in range(0, InventoryScanner.COLUMNS):
+                if (i, j) not in positions_of_missing_items:
+                    item_positions[(i, j)] = 1
+        self.logger.debug(
+            "Found items at inventory cell locations: "+str(item_positions))
+        # Return column sorted.
+        return sorted(item_positions, key=(lambda x: x[1]))
