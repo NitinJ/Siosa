a40dfcc7fdbb1664c08f3b1b10d69dddb75a357e
NitinJ <nitin.jaglan@gmail.com>
2020-09-15 18:42:46 +0530

    Added trading related tasks and helper methods in TradeState to create those states. Also, added a is_transition_valid to state class. This method can be overloaded by child classes to detect invalid state transitions.

--- siosa/dfa/dfa_state.py
+++ siosa/dfa/dfa_state.py
@@ -1,3 +1,4 @@
+import logging
 import threading
 from copy import deepcopy
 
@@ -7,6 +8,8 @@ from siosa.common.decorations import abstractmethod, synchronized
 class DfaState:
     def __init__(self, value):
         self.lock = threading.RLock()
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
         self.value = value
 
     @synchronized
@@ -15,17 +18,27 @@ class DfaState:
 
     @synchronized
     def set_value(self, other):
-        self.value = other
+        if self._is_transition_valid(other):
+            self.value = other
+        else:
+            self.logger.warning("Invalid state transition from {} -> {}"
+                                .format(self.to_string(), str(self.value)))
 
     @synchronized
     def equals(self, other):
-        if other is not None:
-            if isinstance(other, str):
-                return self.to_string() == other
-            return self.to_string() == other.to_string()
-        return False
+        if other is None:
+            return False
+        if isinstance(other, str):
+            return self.to_string() == other
+        if not isinstance(other, DfaState):
+            return False
+        return self.to_string() == other.to_string()
 
     @abstractmethod
     @synchronized
     def to_string(self):
         pass
+
+    @synchronized
+    def _is_transition_valid(self, to):
+        return True
--- siosa/trader/trade_state.py
+++ siosa/trader/trade_state.py
@@ -1,18 +1,86 @@
+from enum import Enum
+
 from siosa.common.decorations import synchronized
 from siosa.dfa.dfa_state import DfaState
 
 
-class TradeState(DfaState):
-    def __init__(self, state_value):
-        super().__init__(state_value)
-
-    def to_string(self):
-        return "{}-{}-{}".format(self.value[0], self.value[1], self.value[2])
-
-
-class States:
+class States(Enum):
+    # Main states
     NOT_STARTED = ('NOT_STARTED', '', '')
     AWAITING_TRADE = ('AWAITING_TRADE', '', '')
-    TRADING_NO_NO = ('TRADING', 'NOT_OFFERED', 'NOT_OFFERED')
     ACCEPTED = ('ACCEPTED', '', '')
     CANCELLED = ('CANCELLED', '', '')
+    LEFT = ('LEFT', '', '')
+    ENDED = ('ENDED', '', '')
+
+    # Trading
+    TRADING_NO_NO = ('TRADING', 'NOT_OFFERED', 'NOT_OFFERED')
+    TRADING_NO_O = ('TRADING', 'NOT_OFFERED', 'OFFERED')
+    TRADING_NO_R = ('TRADING', 'NOT_OFFERED', 'RETRACTED')
+    TRADING_NO_A = ('TRADING', 'NOT_OFFERED', 'ACCEPTED')
+
+    TRADING_O_NO = ('TRADING', 'OFFERED', 'NOT_OFFERED')
+    TRADING_O_O = ('TRADING', 'OFFERED', 'OFFERED')
+    TRADING_O_R = ('TRADING', 'OFFERED', 'RETRACTED')
+    TRADING_O_A = ('TRADING', 'OFFERED', 'ACCEPTED')
+
+    TRADING_VS_NO = ('TRADING', 'VERIFIED_SUCCESS', 'NOT_OFFERED')
+    TRADING_VS_O = ('TRADING', 'VERIFIED_SUCCESS', 'OFFERED')
+    TRADING_VS_R = ('TRADING', 'VERIFIED_SUCCESS', 'RETRACTED')
+    TRADING_VS_A = ('TRADING', 'VERIFIED_SUCCESS', 'ACCEPTED')
+
+    TRADING_VF_NO = ('TRADING', 'VERIFIED_FAIL', 'NOT_OFFERED')
+    TRADING_VF_O = ('TRADING', 'VERIFIED_FAIL', 'OFFERED')
+    TRADING_VF_R = ('TRADING', 'VERIFIED_FAIL', 'RETRACTED')
+    TRADING_VF_A = ('TRADING', 'VERIFIED_FAIL', 'ACCEPTED')
+
+    TRADING_A_NO = ('TRADING', 'ACCEPTED', 'NOT_OFFERED')
+    TRADING_A_O = ('TRADING', 'ACCEPTED', 'OFFERED')
+    TRADING_A_R = ('TRADING', 'ACCEPTED', 'RETRACTED')
+    TRADING_A_A = ('TRADING', 'ACCEPTED', 'ACCEPTED')
+
+    @staticmethod
+    def create_from_trading_state(state_me: str, state_other: str):
+        state = ('TRADING', state_me, state_other)
+        for s in list(States):
+            if s.value == state:
+                return s
+        return None
+
+
+class TradeState(DfaState):
+    STR_FORMAT = "({}_{}_{})"
+
+    def __init__(self, state_value: States):
+        super().__init__(state_value)
+
+    @synchronized
+    def to_string(self):
+        return TradeState.STR_FORMAT.format(self.value.value[0],
+                                            self.value.value[1],
+                                            self.value.value[2])
+
+    @synchronized
+    def equals(self, other):
+        if isinstance(other, States):
+            return self.to_string() == TradeState.STR_FORMAT.format(
+                *other.value)
+        return super().equals(other)
+
+    @synchronized
+    def set_state_me_str(self, state_me: str):
+        self.value = States.create_from_trading_state(
+            state_me, self.value.value[2])
+
+    @synchronized
+    def _is_transition_valid(self, to: States):
+        if not self.value.value[0] == 'TRADING' or \
+                not to.value[0] == 'TRADING':
+            return True
+        # Both are trading.
+        if self.value.value[1] in ['VERIFIED_SUCCESS', 'VERIFIED_FAIL'] \
+                and to.value[1] == 'OFFERED':
+            # Cannot go from verified success/fail to offered. Can only go to
+            # not-offered.
+            return False
+        return True
