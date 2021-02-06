a898245c029780ff325cd1e4dca6dd667953ea61
NitinJ <nitin.jaglan@gmail.com>
2021-02-06 15:23:20 +0530

    Dfa cleanup

--- siosa/dfa/dfa.py
+++ siosa/dfa/dfa.py
@@ -12,14 +12,11 @@ class Dfa(threading.Thread):
         super().__init__()
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
-
         self.state = state
         self.end = end
         self.old_state_str = None
-
         self.fn = {}
         self.running_task = None
-
         self.event_loop = asyncio.new_event_loop()
 
     def add_state_fn(self, s1: DfaState, transition_fn):
@@ -34,30 +31,17 @@ class Dfa(threading.Thread):
             if self.state.equals(self.old_state_str):
                 continue
 
+            # State changed.
             self.logger.debug("State changed from {} to {}".format(
                 self.old_state_str,
                 self.state.to_string()))
             self.old_state_str = self.state.to_string()
 
-            # Stop any running tasks
-            if self.running_task and not self.running_task.done():
-                self.logger.debug(
-                    "Cancelling fn: {}".format(self.running_task.get_name()))
-                self.running_task.cancel()
+            # Stop any running task
+            self._stop_running_task()
 
             # Run the fn for the new state if it's available.
-            new_state_str = self.state.to_string()
-            if new_state_str in self.fn:
-                self.logger.debug(
-                    "Running fn for new state: {}".format(new_state_str))
-                fn = self.fn[new_state_str]
-                try:
-                    self.running_task = \
-                        self.event_loop.create_task(fn(), name=new_state_str)
-                except Exception as err:
-                    self.logger.warning("Exception in dfa while executing "
-                                        "fn for {} : {}".format(
-                        new_state_str, err), stack_info=True, exc_info=True)
+            self._run_fn_for_current_state()
 
             if self.state.equals(self.end):
                 self.logger.debug("End state({}) reached from {}".format(
@@ -67,3 +51,25 @@ class Dfa(threading.Thread):
                 return
 
             await asyncio.sleep(Dfa.SLEEP_DURATION)
+
+    def _stop_running_task(self):
+        if self.running_task and not self.running_task.done():
+            self.logger.debug(
+                "Cancelling fn: {}".format(self.running_task.get_name()))
+            self.running_task.cancel()
+
+    def _run_fn_for_current_state(self):
+        new_state_str = self.state.to_string()
+
+        if new_state_str in self.fn:
+            self.logger.debug(
+                "Running fn for new state: {}".format(new_state_str))
+            fn = self.fn[new_state_str]
+            try:
+                self.running_task = \
+                    self.event_loop.create_task(fn(),
+                                                name=new_state_str)
+            except Exception as err:
+                self.logger.warning("Exception in dfa while executing "
+                                    "fn for {} : {}".format(
+                    new_state_str, err), stack_info=True, exc_info=True)
