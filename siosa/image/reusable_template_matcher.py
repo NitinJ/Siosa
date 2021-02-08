bf55ed1a03f4810599087205b2b8a6877fbc9f67
NitinJ <nitin.jaglan@gmail.com>
2021-02-08 16:59:08 +0530

    Added a reusable template matcher that doesn't take screenshots every time match is called.

---
+++ siosa/image/reusable_template_matcher.py
@@ -0,0 +1,17 @@
+from siosa.image.template import Template
+from siosa.image.template_matcher import TemplateMatcher
+from siosa.image.template_registry import TemplateRegistry
+
+
+class ReusableTemplateMatcher(TemplateMatcher):
+    def __init__(self, location, confidence=0.75, debug=False,
+                 confirm_foreground=False):
+        TemplateMatcher.__init__(self,
+                                 Template.from_registry(TemplateRegistry.STASH),
+                                 confidence=confidence, debug=debug,
+                                 confirm_foreground=confirm_foreground)
+        self.location = location
+
+    def match_template(self, template):
+        self.template = template
+        return self.match(self.location, reuse=True)
