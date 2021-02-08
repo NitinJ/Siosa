12796737503399a2dc5a61e9a4fc03f6d82fd07c
NitinJ <nitin.jaglan@gmail.com>
2021-02-08 17:01:52 +0530

    Added a basic trade verifier. TODO: Add actual currency matching logic

--- siosa/trader/trade_verifier.py
+++ siosa/trader/trade_verifier.py
@@ -1,6 +1,62 @@
+import logging
+
+from siosa.clipboard.poe_clipboard import PoeClipboard
+from siosa.control.mouse_controller import MouseController
+from siosa.image.grid import Grid
+from siosa.image.template import Template
+from siosa.image.template_matcher import TemplateMatcher
+from siosa.image.template_registry import TemplateRegistry
+from siosa.location.location_factory import Locations, LocationFactory
+
+
+
 class TradeVerifier:
+    ROWS = 5
+    COLUMNS = 12
+    BORDER = 2
+
     def __init__(self, trade_info):
         self.trade_info = trade_info
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+        self.lf = LocationFactory()
+        self.mc = MouseController(self.lf)
+        self.clipboard = PoeClipboard()
+
+        self.grid_other = Grid(
+            Locations.TRADE_WINDOW_OTHER,
+            Locations.TRADE_WINDOW_OTHER_0_0,
+            TradeVerifier.ROWS,
+            TradeVerifier.COLUMNS,
+            TradeVerifier.BORDER,
+            TradeVerifier.BORDER)
+        self.trading_tm_other = TemplateMatcher(
+            Template.from_registry(TemplateRegistry.TRADE_WINDOW_OTHER_0_0),
+            confirm_foreground=True)
 
     def verify(self):
-        return True
+        cells_with_items = self.grid_other.get_cells_not_in_positions(
+            self.trading_tm_other.match(
+                self.lf.get(Locations.TRADE_WINDOW_OTHER)))
+        self.logger.debug("Items found at locations : {}".format(
+            cells_with_items))
+        items = []
+        for cell in cells_with_items:
+            cell_center = \
+                self.grid_other.get_cell_location(cell).get_center_location()
+            self.mc.move_mouse(cell_center)
+
+            item = self.clipboard.read_item_at_cursor()
+            if item:
+                items.append(item)
+            else:
+                self.logger.debug("Could not parse item at cell location: {}".format(cell))
+
+        self.logger.debug("Moving mouse to screen no-op position")
+        self.mc.move_mouse(self.lf.get(Locations.SCREEN_NOOP_POSITION))
+
+        return self._verify_currency(items)
+
+    def _verify_currency(self, scanned_items):
+        return len(scanned_items) == 4
