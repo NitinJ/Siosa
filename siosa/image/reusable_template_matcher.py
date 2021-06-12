7203fcd2214dbbac32fc1a44fd2fe4424a3e91cf
NitinJ <nitin.jaglan@gmail.com>
2021-06-12 17:55:27 +0530

    Added support to reset internal cache.

--- siosa/image/reusable_template_matcher.py
+++ siosa/image/reusable_template_matcher.py
@@ -7,11 +7,20 @@ class ReusableTemplateMatcher(TemplateMatcher):
     def __init__(self, location, confidence=0.75, debug=False,
                  confirm_foreground=False, scale=1.0):
         TemplateMatcher.__init__(self,
-                                 Template.from_registry(TemplateRegistry.STASH),
-                                 confidence=confidence, debug=debug,
-                                 confirm_foreground=confirm_foreground)
+                                 None,
+                                 confidence=confidence,
+                                 debug=debug,
+                                 confirm_foreground=confirm_foreground,
+                                 scale=scale)
         self.location = location
 
     def match_template(self, template):
         self.template = template
         return self.match(self.location, reuse=True)
+
+    def reset(self):
+        """
+        Clears the stored location image.
+        Returns: None
+        """
+        self.image_cache = {}
