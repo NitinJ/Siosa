f994b4d17dc2c8050fb92103f4edb0d47a76c0f3
NitinJ <nitin.jaglan@gmail.com>
2021-06-12 17:41:55 +0530

    Added support in registry to get the template image from currency stack information.

--- siosa/image/template_registry.py
+++ siosa/image/template_registry.py
@@ -12,22 +12,26 @@ class TemplateRegistry:
     STASH_CLOSE_BUTTON = ("sct-tmp-STASH_CLOSE_BUTTON.png", (1920, 1080))
     DECORATIONS_BANNER = ("sct-tmp-DECORATIONS_BANNER.png", (1920, 1080))
     AWAITING_TRADE_BOX = ("sct-tmp-TRADE_AWAITING_TRADE_BOX.png", (1920, 1080))
-    AWAITING_TRADE_CANCEL_BUTTON = ("sct-tmp-TRADE_AWAITING_TRADE_CANCEL_BUTTON.png", (1920, 1080))
+    AWAITING_TRADE_CANCEL_BUTTON = (
+        "sct-tmp-TRADE_AWAITING_TRADE_CANCEL_BUTTON.png", (1920, 1080))
     TRADE_WINDOW_TITLE = ("sct-tmp-TRADE_WINDOW_TITLE.png", (1920, 1080))
     TRADE_WINDOW_ME = ("sct-tmp-TRADE_WINDOW_ME.png", (1920, 1080))
     TRADE_WINDOW_ME_0_0 = ("sct-tmp-TRADE_WINDOW_ME_0_0.png", (1920, 1080))
-    TRADE_WINDOW_ME_EMPTY_TEXT = ("sct-tmp-TRADE_WINDOW_ME_EMPTY_TEXT.png", (1920, 1080))
+    TRADE_WINDOW_ME_EMPTY_TEXT = (
+        "sct-tmp-TRADE_WINDOW_ME_EMPTY_TEXT.png", (1920, 1080))
     TRADE_WINDOW_OTHER = ("sct-tmp-TRADE_WINDOW_OTHER.png", (1920, 1080))
-    TRADE_WINDOW_OTHER_0_0 = ("sct-tmp-TRADE_WINDOW_OTHER_0_0.png", (1920, 1080))
+    TRADE_WINDOW_OTHER_0_0 = (
+        "sct-tmp-TRADE_WINDOW_OTHER_0_0.png", (1920, 1080))
     TRADE_WINDOW_OTHER_SMALL_0_0 = (
-    "sct-tmp-TRADE_WINDOW_OTHER_SMALL_0_0.png", (1920, 1080))
-    TRADE_ACCEPT_RETRACTED = ("sct-tmp-TRADE_ACCEPT_RETRACTED.png", (1920, 1080))
+        "sct-tmp-TRADE_WINDOW_OTHER_SMALL_0_0.png", (1920, 1080))
+    TRADE_ACCEPT_RETRACTED = (
+        "sct-tmp-TRADE_ACCEPT_RETRACTED.png", (1920, 1080))
     TRADE_ACCEPT_GREEN_AURA = (
-    "sct-tmp-TRADE_ACCEPT_GREEN_AURA.png", (1920, 1080))
+        "sct-tmp-TRADE_ACCEPT_GREEN_AURA.png", (1920, 1080))
     TRADE_ACCEPT_GREEN_AURA_ME = (
-    "sct-tmp-TRADE_ACCEPT_GREEN_AURA_ME.png", (1920, 1080))
+        "sct-tmp-TRADE_ACCEPT_GREEN_AURA_ME.png", (1920, 1080))
     CANCEL_TRADE_ACCEPT_BUTTON = (
-    "sct-tmp-TRADE_CANCEL_ACCEPT_BUTTON.png", (1920, 1080))
+        "sct-tmp-TRADE_CANCEL_ACCEPT_BUTTON.png", (1920, 1080))
     TRADE_WINDOW_OTHER_0_0_COUNT = (
         "sct-tmp-TRADE_WINDOW_OTHER_0_0_COUNT.png", (1920, 1080))
     TRADE_WINDOW_CLOSE_BUTTON = (
@@ -51,3 +55,7 @@ class TemplateRegistry:
     SIMPLE_SEXTANT_DROP = ("sct-tmp-SIMPLE_SEXTANT_DROP.png", (1920, 1080))
     VAAL_DROP = ("sct-tmp-VAAL_DROP.png", (1920, 1080))
     DROP = ("sct-tmp-DROP.png", (1920, 1080))
+
+    @staticmethod
+    def get_template_for_currency_stack(currency_name, stack_size):
+        return "sct-tmp-{}_{}.png".format(currency_name.upper(), stack_size), (1920, 1080)
