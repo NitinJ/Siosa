bf69034406d96a5f8f9be9084f4a8dd325dd1018
NitinJ <nitin.jaglan@gmail.com>
2021-06-20 22:46:31 +0530

    Cleanup in stash.py

--- siosa/data/stash.py
+++ siosa/data/stash.py
@@ -7,6 +7,39 @@ from siosa.data.stash_tab import StashTabType, StashTab
 from siosa.network.poe_api import PoeApi
 
 
+def _get_stash_type_for_item(item):
+    if not item:
+        return StashTabType.UNKNOWN
+    if item.type == ItemType.CURRENCY:
+        return StashTabType.CURRENCY
+    elif item.type == ItemType.DIVINATION_CARD:
+        return StashTabType.DIVINATION
+    elif item.type in (ItemType.DELIRIUM_ORB,
+                       ItemType.SIMULACRUM_SPLINTER,
+                       ItemType.SIMULACRUM):
+        return StashTabType.DELIRIUM
+    elif item.type in (ItemType.CATALYST, ItemType.ORGAN):
+        return StashTabType.METAMORPH
+    elif item.type == ItemType.OIL:
+        return StashTabType.BLIGHT
+    elif item.type in (ItemType.DELVE_RESONATOR, ItemType.DELVE_FOSSIL):
+        return StashTabType.DELVE
+    elif item.type in (ItemType.SPLINTER,
+                       ItemType.SCARAB,
+                       ItemType.FRAGMENT,
+                       ItemType.OFFERING,
+                       ItemType.DIVINE_VESSEL,
+                       ItemType.TIMELESS_EMBLEM,
+                       ItemType.BREACHSTONE):
+        return StashTabType.FRAGMENT
+    elif item.type == ItemType.ESSENCE:
+        return StashTabType.ESSENCE
+    elif item.type == ItemType.MAP:
+        return StashTabType.MAP
+    else:
+        return StashTabType.UNKNOWN
+
+
 class Stash(metaclass=Singleton):
     def __init__(self):
         self.logger = logging.getLogger(__name__)
@@ -14,7 +47,7 @@ class Stash(metaclass=Singleton):
 
         self.config = SiosaConfig().config
         self.poe_api = PoeApi()
-        self.name_to_stash_tab = {}
+        self.name_to_stash_tabs = {}
         self.tab_type_to_tabs = {}
         self.tabs = {}
         self.stash_metadata = None
@@ -26,6 +59,14 @@ class Stash(metaclass=Singleton):
         self._populate_internal()
 
     def get_stash_tab_by_index(self, index):
+        """
+        Returns the stash tab at a given index.
+        Args:
+            index: Index of stash tab to return
+
+        Returns: Stash tab
+
+        """
         if index not in self.tabs.keys():
             self.logger.error("Invalid stash tab index={}".format(index))
             return None
@@ -37,26 +78,26 @@ class Stash(metaclass=Singleton):
         Args:
             name: Name of the stash tabs
         """
-        if name not in self.name_to_stash_tab.keys():
+        if name not in self.name_to_stash_tabs.keys():
             self.logger.error("Stash tab with name={} is not present in Stash"
                               .format(name))
             return None
-        # Return first stash tab in case there are multiple by the same name.
-        return self.name_to_stash_tab[name]
+        return self.name_to_stash_tabs[name]
 
     def get_stash_tabs_for_item(self, item):
         """
-        Returns the stash tabs to which the given item belongs.
+        Returns a list of stash tabs to which the given item belongs.
         Args:
             item: Item
         """
-        stash_tab_type = self._get_stash_type_for_item(item)
+        stash_tab_type = _get_stash_type_for_item(item)
         self.logger.info("Stash tab type={} for item_type=({})".format(
             stash_tab_type, item.type))
 
         if stash_tab_type == StashTabType.CURRENCY:
             return self.get_currency_stash_tabs()
-        elif stash_tab_type == StashTabType.UNKNOWN or stash_tab_type not in self.tab_type_to_tabs:
+        elif stash_tab_type == StashTabType.UNKNOWN or \
+                stash_tab_type not in self.tab_type_to_tabs:
             return self.get_dump_stash_tabs()
         return self.tab_type_to_tabs[stash_tab_type]
 
@@ -78,10 +119,10 @@ class Stash(metaclass=Singleton):
                 'is_premium': is_premium
             })
             self.logger.debug("Creating stash-tab {}".format(str(stash_tab)))
-            if name not in self.name_to_stash_tab:
-                self.name_to_stash_tab[name] = [stash_tab]
+            if name not in self.name_to_stash_tabs:
+                self.name_to_stash_tabs[name] = [stash_tab]
             else:
-                self.name_to_stash_tab[name].append(stash_tab)
+                self.name_to_stash_tabs[name].append(stash_tab)
 
             stash_type = stash_tab.get_type()
             if stash_type not in self.tab_type_to_tabs:
@@ -100,39 +141,6 @@ class Stash(metaclass=Singleton):
 
     def _get_all_stashes_with_names(self, tab_names):
         stash_tabs = []
-        for name in tab_names:
-            stash_tabs.extend(self.get_stash_tabs_by_name(name))
+        for tab_name in tab_names:
+            stash_tabs.extend(self.get_stash_tabs_by_name(tab_name))
         return stash_tabs
-
-    def _get_stash_type_for_item(self, item):
-        if not item:
-            return StashTabType.UNKNOWN
-        if item.type == ItemType.CURRENCY:
-            return StashTabType.CURRENCY
-        elif item.type == ItemType.DIVINATION_CARD:
-            return StashTabType.DIVINATION
-        elif item.type in (ItemType.DELIRIUM_ORB,
-                           ItemType.SIMULACRUM_SPLINTER,
-                           ItemType.SIMULACRUM):
-            return StashTabType.DELIRIUM
-        elif item.type in (ItemType.CATALYST, ItemType.ORGAN):
-            return StashTabType.METAMORPH
-        elif item.type == ItemType.OIL:
-            return StashTabType.BLIGHT
-        elif item.type in (ItemType.DELVE_RESONATOR, ItemType.DELVE_FOSSIL):
-            return StashTabType.DELVE
-        elif item.type in (ItemType.SPLINTER,
-                           ItemType.SCARAB,
-                           ItemType.FRAGMENT,
-                           ItemType.OFFERING,
-                           ItemType.DIVINE_VESSEL,
-                           ItemType.TIMELESS_EMBLEM,
-                           ItemType.BREACHSTONE):
-            return StashTabType.FRAGMENT
-        elif item.type == ItemType.ESSENCE:
-            return StashTabType.ESSENCE
-        elif item.type == ItemType.MAP:
-            return StashTabType.MAP
-        else:
-            return StashTabType.UNKNOWN
-
