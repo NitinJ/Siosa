a49d9fadb194f06d0c98d0907e9b6fe7682f49a1
NitinJ <nitin.jaglan@gmail.com>
2021-06-22 21:09:44 +0530

    Minor fix

--- siosa/data/stash_item.py
+++ siosa/data/stash_item.py
@@ -1,7 +1,7 @@
 from siosa.data.stash import StashTab
 
 
-class StashItem():
+class StashItem:
     def __init__(self, item, stash_tab: StashTab, position):
         self.item = item
         self.stash_tab = stash_tab
