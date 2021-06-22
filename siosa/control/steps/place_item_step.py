202fb54c55590511f60a18eb25ff2ed2c8cec753
NitinJ <nitin.jaglan@gmail.com>
2021-06-22 21:12:41 +0530

    Added a place item step that places a item in an inventory cell into a stash tab.

---
+++ siosa/control/steps/place_item_step.py
@@ -0,0 +1,67 @@
+from enum import Enum
+
+from siosa.clipboard.poe_clipboard import PoeClipboard
+from siosa.control.game_step import Step, StepStatus
+from siosa.data.inventory import Inventory
+from siosa.data.stash import Stash
+from siosa.location.location_factory import Locations
+
+
+class Error(Enum):
+    STASH_NOT_OPEN = 0
+    ITEM_NOT_FOUND_IN_INVENTORY_CELL = 1
+    STASH_TAB_NOT_OPEN = 2
+    ITEM_ALREADY_EXISTS_IN_STASH = 3
+    COULD_NOT_PLACE_ITEM_IN_STASH = 4
+
+
+class PlaceItem(Step):
+    def __init__(self, stash_index, stash_cell, item_cell):
+        Step.__init__(self)
+        self.stash_index = stash_index
+        self.stash_cell = stash_cell
+        self.stash_tab = Stash().get_stash_tab_by_index(stash_index)
+        assert self.stash_tab is not None
+
+        self.item_cell = item_cell
+        assert Inventory.is_in_bounds(item_cell)
+
+        self.poe_clipboard = PoeClipboard()
+
+    def execute(self, game_state):
+        self.game_state = game_state.get()
+        if not self.game_state['stash_open']:
+            return StepStatus(False, Error.STASH_NOT_OPEN)
+        if self.game_state['open_stash_tab_index'] != self.stash_index:
+            return StepStatus(False, Error.STASH_TAB_NOT_OPEN)
+
+        # Check if item is present in cell and not present in stash cell.
+        item = Inventory.get_item_at_cell(self.item_cell, get_dimensions=True)
+        if not item:
+            return StepStatus(False, Error.ITEM_NOT_FOUND_IN_INVENTORY_CELL)
+        if self.stash_tab.is_item_at_location_ingame(
+                self.stash_cell[0], self.stash_cell[1]):
+            return StepStatus(False, Error.ITEM_ALREADY_EXISTS_IN_STASH)
+
+        # Pickup item from cell.
+        self.mc.move_mouse(Inventory.get_location(self.item_cell))
+        self.mc.click()
+
+        # Move to location in the stash tab.
+        location = self.stash_tab.get_cell_location_for_placing_item(
+            self.stash_cell, item.get_dimensions())
+        self.mc.move_mouse(location)
+        # self.mc.click()
+        self.mc.move_mouse(self.lf.get(Locations.SCREEN_NOOP_POSITION))
+
+        if not self.stash_tab.is_item_at_location_ingame(
+                self.stash_cell[1], self.stash_cell[0]):
+            # Somehow the item isn't in the stash tab, so place it back where we
+            # picked it up from.
+            location = Inventory.get_location_for_placing_item(item,
+                (self.item_cell[0], self.item_cell[1]))
+            self.mc.click_at_location(location)
+            self.logger.info("Placed item back into the inventory")
+            return StepStatus(False, Error.COULD_NOT_PLACE_ITEM_IN_STASH)
+
+        return StepStatus(True)
