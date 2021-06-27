382ffa8d0bbf8f01bcf761cd4d977de2a08518b3
NitinJ <nitin.jaglan@gmail.com>
2021-06-28 00:18:13 +0530

    Created a GGPK directory to hold data parsed from content.ggpk file. Added data for base_items and it's corresponding reader class. THe json file is generated from RePoE project (which internally uses PyPoE to parse ggpk files)

---
+++ siosa/resources/ggpk_data/base_items.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+
+class BaseItems:
+    '''
+    Resource file with metadata for all base items. Sample entry:
+
+    '''
+    _items = {}
+    FILE_NAME = "base_items.json"
+
+    @staticmethod
+    def _get_path(filename):
+        return os.path.join(os.path.dirname(__file__), filename)
+
+    @staticmethod
+    def get_item_dimensions(item_name):
+        if not BaseItems._items:
+            BaseItems._preprocess()
+        if item_name not in BaseItems._items.keys():
+            return None
+        data = BaseItems._items[item_name]
+        return data['inventory_width'], data['inventory_height']
+
+    @staticmethod
+    def _preprocess():
+        data = json.load(open(BaseItems._get_path(BaseItems.FILE_NAME), 'r'))
+        for k,v in data.items():
+            if "Metadata" not in k:
+                continue
+            BaseItems._items[v['name']] = {
+                'inventory_height' : v['inventory_height'],
+                'inventory_width' : v['inventory_width']
+            }
