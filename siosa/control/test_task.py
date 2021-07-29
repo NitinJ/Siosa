60514ebd56ecd7c123ab097df273526d29646ebb
NitinJ <nitin.jaglan@gmail.com>
2021-07-29 12:05:23 +0530

    Changes to test task

--- siosa/control/test_task.py
+++ siosa/control/test_task.py
@@ -6,25 +6,27 @@ from siosa.control.steps.test_step import TestStep
 
 
 class TestTask(Task):
-    def __init__(self, priority):
+    def __init__(self, priority, sleep_time=1.0, name="TestTask"):
         """
         Args:
             priority:
         """
         self.logger = logging.getLogger(__name__)
         Task.__init__(self, priority, name=__name__)
+        self.sleep_time = sleep_time
+        self.name = name
 
     def get_steps(self):
         """Generator function for step execution logic. Returns: A generator
         function for steps.
         """
-        yield TestStep(2)
-        if self._get_last_step_result().info == 1:
-            yield TestStep(2)
-        else:
-            yield TestStep(3)
+        yield TestStep(
+            self.sleep_time,
+            fail=False,
+            name='{}: TestStep({})'.format(self.name, 1))
+        return True
 
 
 if __name__ == "__main__":
-    t = TestTask(10)
+    t = TestTask(11)
     t.run_task(GameState())
