aeb93257b6fb4a9d7594140ba0b7e3a57fb0515a
NitinJ <nitin.jaglan@gmail.com>
2021-07-31 15:54:46 +0530

    Added an exception in clipboardReader where sometimes data with wrong format gets copied onto the clipboard. In this case clipboard reader raise a CLipboardDataFormatException and the client is expected to put data onto the clipboard again.

--- siosa/clipboard/clipboard_reader.py
+++ siosa/clipboard/clipboard_reader.py
@@ -6,7 +6,14 @@ import win32clipboard
 from siosa.common.singleton import Singleton
 
 
+class ClipboardDataFormatException(Exception):
+    def __init__(self):
+        super().__init__()
+
+
 class ClipboardReader(metaclass=Singleton):
+    FORMAT_ERROR = "Specified clipboard format is not available"
+
     def __init__(self):
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel('DEBUG')
@@ -33,10 +40,12 @@ class ClipboardReader(metaclass=Singleton):
                 data = win32clipboard.GetClipboardData()
                 win32clipboard.CloseClipboard()
                 return data
-            except Exception as e:
+            except RuntimeError as e:
                 self.logger.error(
                     "Error while reading clipboard: {}".format(str(e)))
-            time.sleep(0.01)
+                if ClipboardReader.FORMAT_ERROR in str(e):
+                    raise ClipboardDataFormatException()
+            time.sleep(0.05)
 
     def _write_internal(self, data):
         while True:
