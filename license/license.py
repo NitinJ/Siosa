a5641f2f8589a9143da8372f772f87a9e04506d3
NitinJ <nitin.jaglan@gmail.com>
2021-08-02 01:18:00 +0530

    Added code to get and register keys.

--- license/license.py
+++ license/license.py
@@ -1,6 +1,23 @@
+import json
 import logging
+import subprocess
+
+import requests
 
 from siosa.config.siosa_config import SiosaConfig
+
+MID_PROC = "wmic csproduct get uuid"
+SERVER = "https://siosa-poe.com"
+REGISTER = "/api/v1/licenses/register"
+VERIFY = "/api/v1/licenses/verify"
+
+
+def _get_muid():
+    try:
+        return \
+            subprocess.check_output(MID_PROC).decode().split('\n')[1].strip()
+    except:
+        return None
 
 
 class License:
@@ -13,11 +30,43 @@ class License:
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
 
-    def valid(self):
+    def valid(self, _key=None):
         self.logger.debug("Validating license")
-        key = self.config.get_license_key()
+        key = self.config.get_license_key() if not _key else _key
         if not key:
             self.logger.info("License key not set !")
             return False
-        # TODO: Call license server and check validity
-        return True
+
+        muid = _get_muid()
+        if not muid:
+            return False
+
+        resp = requests.post(SERVER + VERIFY,
+                             json={'license_key': key, "muid": muid})
+        if resp:
+            return True
+        return False
+
+    def register(self, key):
+        """
+
+        Args:
+            key: The new key to register.
+
+        Returns: Whether the operation succeeded.
+
+        """
+        if not key:
+            return False
+
+        muid = _get_muid()
+        if not muid:
+            return False
+
+        resp = requests.post(SERVER + REGISTER,
+                             json={'license_key': key, "muid": muid})
+
+        if resp:
+            self.config.set_license_key(key)
+            return True
+        return False
