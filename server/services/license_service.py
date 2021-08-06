bd52acd95aba2a749c413ad16250c92efe346a00
NitinJ <nitin.jaglan@gmail.com>
2021-08-07 02:44:59 +0530

    License service changes as per License changes. Service now updates the config as well

--- server/services/license_service.py
+++ server/services/license_service.py
@@ -5,16 +5,18 @@ from siosa.config.siosa_config import SiosaConfig
 class LicenseService:
     def __init__(self, siosa_config: SiosaConfig):
         self.config = siosa_config
-        self.license = License(self.config)
 
     def get(self):
         return {
             "key": self.config.get_license_key(),
-            "valid": self.license.valid()
+            "valid": License.valid(self.config.get_license_key())
         }
 
     def register(self, key):
+        valid = License.register(key)
+        if valid:
+            self.config.update({"license_key": key})
         return {
             "key": key,
-            "valid": self.license.register(key)
+            "valid": valid
         }
