e1457933d1e79c1585e9ef954cfd40e6b3f0cc22
NitinJ <nitin.jaglan@gmail.com>
2021-08-11 19:35:40 +0530

    1. Added method to stop all tasks and 2. Config/get now returns stashmetadata s well. 3. Renamed task/create/roll to task/create/roller

--- server/siosa_server.py
+++ server/siosa_server.py
@@ -42,7 +42,7 @@ def create_trade_task():
     return {"status": task_service.create_trade_task()}
 
 
-@app.route("/task/create/roll", methods=['POST'])
+@app.route("/task/create/roller", methods=['POST'])
 def create_roll_task():
     config = request.get_json()
     if not config:
@@ -64,16 +64,35 @@ def get_task():
     return {"status": True, "details": task_service.get_task()}
 
 
+@app.route("/task/stop", methods=['POST'])
+def stop_task():
+    task_service = service_manager.get_service(ServiceType.TASK)
+    if not task_service:
+        return {"status": False}
+
+    return {"status": True, "details": task_service.stop_all_tasks()}
+
 # ##############################################################################
 # Config
 @app.route("/config/get", methods=['GET'])
 def get_config():
     config_service = service_manager.get_service(ServiceType.CONFIG)
-    if not config_service:
+    metadata_service = service_manager.get_service(ServiceType.METADATA)
+    if not config_service or not metadata_service:
         return {"status": False}
 
-    return {"status": True, "details": {"config": config_service.get(),
-                                        "metadata": get_metadata()}}
+    config_data = config_service.get()
+    stashes = metadata_service.get_stashes()
+    leagues = metadata_service.get_leagues()
+    config_metadata = get_metadata()
+    for field in config_metadata:
+        if field['name'] in ('sell', 'dump', 'currency'):
+            field['values'] = [stash['n'] for stash in stashes]
+        if field['name'] == 'league':
+            field['values'] = [league['id'] for league in leagues]
+
+    config_data.update({"metadata": config_metadata})
+    return {"status": True, "details": config_data}
 
 
 @app.route("/config/update", methods=['POST'])
@@ -87,7 +106,7 @@ def update_config():
         return {"status": False}
 
     return {"status": True,
-            "details": {"config": config_service.update(config)}}
+            "details": config_service.update(config)}
 
 
 # ##############################################################################
