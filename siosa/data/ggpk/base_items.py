c90ddd781b42b8efd4cbfe9ab735c371c7b0d07a
NitinJ <nitin.jaglan@gmail.com>
2021-08-25 01:29:22 +0530

    Updated code to fetch baseitems data

--- siosa/data/ggpk/base_items.py
+++ siosa/data/ggpk/base_items.py
@@ -19,24 +19,11 @@ class BaseItems:
         return os.path.join(siosa_base, "resources/ggpk_data/{}".format(filename))
 
     @staticmethod
-    def get_item_dimensions(item_name):
-        """
-        Args:
-            item_name:
-        """
-        item_name = item_name.strip().lower()
-        if not BaseItems._items:
-            BaseItems._preprocess()
-        if item_name not in BaseItems._items.keys():
-            return None
-        data = BaseItems._items[item_name]
-        return data['inventory_width'], data['inventory_height']
-
-    @staticmethod
     def get(item_name):
-        item_name = item_name.strip().lower()
         if not BaseItems._items:
             BaseItems._preprocess()
+
+        item_name = item_name.strip().lower()
         if item_name not in BaseItems._items.keys():
             return None
         return BaseItems._items[item_name]
@@ -44,13 +31,11 @@ class BaseItems:
     @staticmethod
     def _preprocess():
         data = json.load(open(BaseItems._get_path(BaseItems.FILE_NAME), 'r'))
-        for k,v in data.items():
-            if "Metadata" not in k:
-                continue
-            BaseItems._items[v['name'].lower()] = {
-                'inventory_height' : v['inventory_height'],
-                'inventory_width' : v['inventory_width'],
-                'item_class': v['item_class']
+        for v in data['data']:
+            BaseItems._items[v['Name'].lower()] = {
+                'inventory_height': v['Height'],
+                'inventory_width': v['Width'],
+                'item_class': v['Class'].split("/")[-1]
             }
 
 
