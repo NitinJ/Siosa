25d0d5e29a7bc77afd4aa4624d1788cee37549cd
NitinJ <nitin.jaglan@gmail.com>
2021-10-12 17:58:31 +0530

    Moved stash tab scanner to use contours for scanning tab ingame.

--- siosa/image/stash_tab_scanner.py
+++ siosa/image/stash_tab_scanner.py
@@ -1,12 +1,20 @@
 import logging
 
+from cv2 import cv2
+
+from siosa.data.stash_cell_locations import StashCellLocation
 from siosa.image.grid import Grid
-from siosa.image.template import Template
-from siosa.image.template_matcher import TemplateMatcher
-from siosa.image.template_registry import TemplateRegistry
+from siosa.image.utils import grab_screenshot, grayscale, invert
 from siosa.location.location import Location
 from siosa.location.location_factory import LocationFactory, Locations
-from siosa.data.stash_cell_locations import StashCellLocation
+
+
+def process_inventory_image(img):
+    ret, threshold = cv2.threshold(img, 20, 255, cv2.THRESH_BINARY)
+    image = grayscale(threshold)
+    ret, image = cv2.threshold(image, 20, 255, cv2.THRESH_BINARY)
+    image = invert(image)
+    return image
 
 
 class StashTabScanner:
@@ -26,6 +34,8 @@ class StashTabScanner:
         self.stash_tab = stash_tab
         self.cell_0_0 = Locations.STASH_QUAD_0_0 if self.stash_tab.is_quad \
             else Locations.STASH_NORMAL_0_0
+        self.min_contour_area = 500 if self.stash_tab.is_quad else 2000
+        self.debug = debug
         self.grid = Grid(
             Locations.STASH_TAB,
             self.cell_0_0,
@@ -34,14 +44,33 @@ class StashTabScanner:
             self.stash_tab.cell_border_x,
             self.stash_tab.cell_border_y)
 
-        template = TemplateRegistry.QUAD_STASH_0_0 if \
-            self.stash_tab.is_quad else TemplateRegistry.NORMAL_STASH_0_0
-        self.tm = TemplateMatcher(template.get(), debug=debug, threshold=0.75)
+    def _get_empty_cells_using_contours(self):
+        image_original = grab_screenshot(self.lf.get(Locations.STASH_TAB))
+        image = process_inventory_image(image_original)
+        contours, hierarchy = cv2.findContours(
+            image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        emtpy_cells = []
+        for c in contours:
+            if cv2.contourArea(c) > self.min_contour_area:
+                x, y, w, h = cv2.boundingRect(c)
+                center = (x + w // 2, y + h // 2)
+                if self.debug:
+                    image = cv2.rectangle(
+                        image_original, center, center, (0, 0, 255), 4)
+                emtpy_cells.append(center)
+
+        if self.debug:
+            cv2.imshow('Contours', image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+        return emtpy_cells
 
     def get_item_cells(self):
         """Returns: Cells at which items are present."""
         return self.grid.get_cells_not_in_positions(
-            self.tm.match(self.lf.get(Locations.STASH_TAB)))
+            self._get_empty_cells_using_contours())
 
     def is_empty(self, cell):
         """
@@ -50,9 +79,8 @@ class StashTabScanner:
         """
         self.logger.debug(
             "Checking if cell ({}, {}) is empty".format(cell[0], cell[1]))
-        empty_cells = \
-            self.grid.get_cells_in_positions(
-                self.tm.match(self.lf.get(Locations.STASH_TAB)))
+        empty_cells = self.grid.get_cells_in_positions(
+            self._get_empty_cells_using_contours())
         if cell in empty_cells:
             self.logger.debug("Cell({}, {}) is empty".format(cell[0], cell[1]))
             return True
@@ -93,7 +121,6 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format="[%(filename)s:%(lineno)s - %(funcName)s() ] %(message)s")
 
-
     class StashTabFake:
         def __init__(self, is_quad, size, cell_border):
             self.is_quad = is_quad
@@ -101,7 +128,7 @@ if __name__ == "__main__":
             self.cell_border_x = cell_border[0]
             self.cell_border_y = cell_border[1]
 
-
     s = StashTabFake(True, (24, 24), (1, 1))
     scanner = StashTabScanner(s, debug=True)
-    print(scanner.is_empty((10, 18)))
+    scanner.get_item_cells()
+    print(scanner.is_empty((5, 5)))
