12b68e198f698629c45da1fe7b4a5cade3fdb04a
NitinJ <nitin.jaglan@gmail.com>
2021-10-27 14:27:55 +0530

    Updated ggpk data updater to read from RePOE generated files

--- tools/ggpk_data_updater.py
+++ tools/ggpk_data_updater.py
@@ -1,27 +1,27 @@
+"""
+Updates the data files. These are extracted from ggpk files using RePOE.
+This needs to run whenever POE's ggpk data file is updated with new base items,
+item classes, as these are the data items used in Siosa.
+Steps-
+1. Remove item blacklist from RePOE (base_items.py)
+2. Run RePoe (python run_parser.py all)
+3. Run this tool with the path to base_items.json as argument
+"""
+import argparse
 import json
 import logging
 import os
 import sys
 
-import requests
-
 from siosa.common.util import parent
 
 FORMAT = "%(created)f - %(thread)d: [%(filename)s:%(lineno)s - %(funcName)s() ] %(message)s"
-logging.basicConfig(level=logging.INFO, format=FORMAT)
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 logger = logging.getLogger()
 
-VERSION_URL = "https://poedat.erosson.org/pypoe/v1/latest.json"
-BASE_ITEMS_URL = "https://poedat.erosson.org/pypoe/v1/tree/{version}/default/BaseItemTypes.dat.min.json"
 BASE_ITEMS_FILE_NAME = "base_items.json"
 ITEM_CLASSES_FILE_NAME = "item_classes.json"
-ITEM_CLASS_BLACKLIST = [
-     "LabyrinthTrinket",
-     "AbstactPantheonSoul",
-     "Leaguestone",
-     "AbstractMicrotransaction",
-     "AbstractHideoutDoodad"
-]
+
 
 def get_base_items_file_path(fname):
     siosa_base = parent(parent(__file__))
@@ -29,53 +29,24 @@ def get_base_items_file_path(fname):
         siosa_base, "siosa/resources/ggpk_data/{}".format(fname))
 
 
-def get_latest_version():
-    resp = requests.get(VERSION_URL)
-    if not resp.content:
-        return None
-    data = json.loads(resp.content)
-    if not data or 'version' not in data:
-        return None
-    return data['version']
-
-
-def get_current_version():
-    file_path = get_base_items_file_path(BASE_ITEMS_FILE_NAME)
-    if not os.path.exists(file_path):
-        return None
-    data = json.load(open(file_path, 'r'))
-    if not data or 'version' not in data:
-        return None
-    return data['version']
-
-
-def fetch_and_update_base_items(version):
-    url = BASE_ITEMS_URL.format(version=version)
-    resp = requests.get(url)
-    if not resp or not resp.content:
-        return False
-    data = json.loads(resp.content)
-    if not data or 'data' not in data:
+def update_base_items(base_items_file_handle):
+    data = json.load(base_items_file_handle)
+    if not data:
         return False
     output = {
-        'version': version,
-        'filename': data['filename'],
         'data': []
     }
     class_names = set()
-    for item in data['data']:
-        if any([cls in item[5] for cls in ITEM_CLASS_BLACKLIST]):
-            continue
-
+    for key, item in data.items():
         output_item = {
-            'id': item[0],
-            'Width': item[2],
-            'Height': item[3],
-            'Name': item[4],
-            'Class': item[5]
+            'id': key,
+            'Width': item["inventory_width"],
+            'Height': item["inventory_height"],
+            'Name': item["name"],
+            'Class': item["item_class"].replace(" ", ""),
         }
         output['data'].append(output_item)
-        class_names.add(item[5].split("/")[-1])
+        class_names.add(output_item["Class"])
     json.dump(
         output, open(get_base_items_file_path(BASE_ITEMS_FILE_NAME), 'w'))
     json.dump(
@@ -85,22 +56,18 @@ def fetch_and_update_base_items(version):
 
 
 if __name__ == "__main__":
-    overwrite = True
+    parser = argparse.ArgumentParser(description='Updates ggpk data for Siosa '
+                                                 'using RePOE generated json')
+    parser.add_argument('base_items_json_path',
+                        type=argparse.FileType('r', encoding='UTF-8'),
+                        help='Path to base_items.json file')
 
-    latest_version = get_latest_version()
-    if not latest_version:
-        logger.error("Couldn't get latest version from {}".format(VERSION_URL))
+    args = parser.parse_args()
+    base_items_file_path = args.base_items_json_path
+    success = update_base_items(base_items_file_path)
+    if success:
+        logger.debug("Successfully updated ggpk data".format())
+    else:
+        logger.debug("Error: Couldn't update ggpk data")
         sys.exit(1)
 
-    current_version = get_current_version()
-    if latest_version == current_version and not overwrite:
-        logger.debug("Already at latest version !")
-        sys.exit(0)
-
-    success = fetch_and_update_base_items(latest_version)
-    if success:
-        logger.debug("Successfully updated to version: {}".format(
-            latest_version))
-    else:
-        logger.debug(
-            "Error: Couldn't update to version: {}".format(latest_version))
