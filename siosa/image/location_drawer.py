a84a00c2121ab4f34294a88bb2ff48e36b4bc39d
NitinJ <nitin.jaglan@gmail.com>
2021-10-30 22:10:11 +0530

    Changes to location drawer. Can draw matches on images from disk now

--- siosa/image/location_drawer.py
+++ siosa/image/location_drawer.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import cv2.cv2 as cv2
 import mss
@@ -6,25 +7,22 @@ import mss.tools
 import numpy as np
 from PIL import Image
 from siosa.location.location_factory import LocationFactory, Locations
+from siosa.location.resolution import Resolution
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class LocationDrawer:
-    def __init__(self):
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.DEBUG)
+    @staticmethod
+    def get_image(image_path):
+        if not image_path or not os.path.exists(image_path):
+            logger.debug("Reading image from screen.")
+            return LocationDrawer.get_image_from_screen()
+        return cv2.imread(image_path)
 
     @staticmethod
-    def draw(location):
-        """Draws the given location on screen :param location: The location of
-        the screen to draw a rectangle on.
-
-        Args:
-            location:
-        """
-
-        # The screen part to capture
-        image = None
-
+    def get_image_from_screen():
         lf = LocationFactory()
         full_screen_grab_params = LocationDrawer._get_grab_params(
             lf.get(Locations.SCREEN_FULL))
@@ -36,14 +34,35 @@ class LocationDrawer:
             (lf.resolution.w, lf.resolution.h),
             image.rgb)
 
-        image_bytes_bgr = cv2.cvtColor(
-            np.array(image_bytes_rgb), cv2.COLOR_RGB2BGR)
-        cv2.rectangle(image_bytes_bgr, (location.x1, location.y1), (location.x2, location.y2), (0, 0, 255), 2)
+        return cv2.cvtColor(np.array(image_bytes_rgb), cv2.COLOR_RGB2BGR)
 
-        image_bytes_bgr = cv2.resize(image_bytes_bgr,  (lf.resolution.w//1, lf.resolution.h//1))
+    @staticmethod
+    def get_location_factory(image_path):
+        if not image_path:
+            return LocationFactory()
+        image = cv2.imread(image_path)
+        h,w = image.shape[0:2]
+        return LocationFactory(resolution=Resolution(w, h))
+
+    @staticmethod
+    def draw(location, image_path=None):
+        """Draws the given location on screen :param location: The location of
+        the screen to draw a rectangle on.
+
+        Args:
+            location:
+        """
+        image = LocationDrawer.get_image(image_path)
+        lf = LocationDrawer.get_location_factory(image_path)
+        location = lf.get(location)
+
+        # Draw rectangle around image
+        cv2.rectangle(image, (location.x1, location.y1),
+                      (location.x2, location.y2),
+                      (0, 255, 102), 4)
 
         cv2.namedWindow("image", cv2.WINDOW_NORMAL)
-        cv2.imshow('image', image_bytes_bgr)
+        cv2.imshow('image', image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
